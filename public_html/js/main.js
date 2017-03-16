aea16e8617d8412bb5a51f7d92e5ccb92cba6bca
bojant987 <bojant987@hotmail.com>
2017-03-16 14:27:34 +0100

    header done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+
+    // marquee init
+    $('.marquee').marquee({
+        duration: 15000,
+        gap: 50,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: false
+    });
+
+    // navigaation dropdown
+    if ($(window).width() < 992) {
+        $(".dropdown-control").click(function(e) {
+            e.preventDefault();
+            $(".dropdown-list").slideToggle();
+            $(this).find(".fa").toggleClass("fa-plus");
+            $(this).find(".fa").toggleClass("fa-minus");
+            console.log($(window).width());
+        });
+    }
 
 
-
+});
