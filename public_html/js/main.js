8b2f0b83341239bb86522a6b7a41848b03b9a25b
bojant987 <bojant987@hotmail.com>
2017-03-18 00:58:09 +0100

    sliders done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -10,15 +10,62 @@ $(document).ready(function() {
     });
 
     // navigaation dropdown
-    if ($(window).width() < 992) {
-        $(".dropdown-control").click(function(e) {
-            e.preventDefault();
+    $(".dropdown-control").click(function(e) {
+        e.preventDefault();
+        if ($(window).width() < 992) {
             $(".dropdown-list").slideToggle();
+
             $(this).find(".fa").toggleClass("fa-plus");
             $(this).find(".fa").toggleClass("fa-minus");
-            console.log($(window).width());
-        });
-    }
+        }
 
+
+    });
+
+
+    // counter bar counter splitting into digits
+    $(".counter-bar .counter-input").each(function() {
+        var input = $(this).text();
+
+        for (var i = 0; i < input.length; i++) {
+            $(this).parent().append("<span>" + input[i] + "</span>");
+        }
+    });
+
+    // sliders
+    $(".kids-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false
+    });
+
+    $(".grownups-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false
+    });
+
+    $(".funds-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false
+    });
+
+    $(".hospitals-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false
+    });
+
+    $(".specials-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false
+    });
 
 });
