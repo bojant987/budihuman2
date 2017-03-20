2ffcad05b85939f127aaaf2cb4208f4f58fa1ee4
bojant987 <bojant987@hotmail.com>
2017-03-20 23:20:32 +0100

    index page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
 
     // counter bar counter splitting into digits
-    $(".counter-bar .counter-input").each(function() {
+    $(".counter-input").each(function() {
         var input = $(this).text();
 
         for (var i = 0; i < input.length; i++) {
@@ -67,5 +67,80 @@ $(document).ready(function() {
         auto: true,
         autoHover: false
     });
+
+    $(".ambassadors-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false,
+        controls: false
+    });
+
+    $(".twitter-ambassadors-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false,
+        controls: false
+    });
+
+    $(".news-slider").slippry({
+        pager: false,
+        transition: "horizontal",
+        auto: true,
+        autoHover: false,
+        controls: true
+    });
+
+    // thermometer
+    function thermometer() {
+        var min = parseFloat($(".thermometer").attr("data-min"));
+        var max = parseFloat($(".thermometer").attr("data-max"));
+        var value = parseFloat($(".thermometer").attr("data-value"));
+        min = min.toFixed(2);
+        max = max.toFixed(2);
+        value = value.toFixed(2);
+        var missing = (max - value).toFixed(2);
+        var progress = (value / (max - min)) * 100;
+
+        $(".thermometer .progress-bar").css("width", progress + "%");
+        $(".thermometer .progress-bar + span").css("left", (progress * 0.96) + "%");
+
+        $(".current > span:nth-of-type(2)").text(thousandSeparator(value) + " din");
+        $(".missing > span:nth-of-type(2)").text(thousandSeparator(missing) + " din");
+        $(".needed > span:nth-of-type(2)").text(thousandSeparator(max) + " din");
+    }
+    thermometer();
+
+    // thousand separator
+    function thousandSeparator(number) {
+        number = number.toString().replace(".", ",");
+        var result = number.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        return result;
+    }
+
+    // table numbers display
+    function tableDisplay() {
+
+        $(".last-donations table .table-input").each(function(input) {
+            var output = parseFloat($(this).text()).toFixed(2);
+            $(this).next().text(thousandSeparator(output) + " din");
+
+        });
+    }
+    tableDisplay();
+
+    // handle terrible picture sizes in news-slider
+    function fixAspectRatio() {
+        $(".news-slider img").each(function() {
+            var ratio = 1.71746;
+            var imgWidth = $(this).width();
+            var imgHeight = imgWidth / ratio;
+            $(this).height(imgHeight);
+        });
+    }
+    fixAspectRatio();
+    // ddd
+
 
 });
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+
+
+// app.get('/', function(req, res){
+// res.render('index.ejs');
+// });
+
+
+app.use(express.static(__dirname + '/public_html'));
+
+app.listen(4000, function() {
+    console.log('Example app listening on port 4000!')
+})
